4f7031da2a5264a27682bd0558ae87c83b045b3f
jb <jessb.epicodus@gmail.com>
2022-01-12 15:51:56 -0800

    WIP: forms created; working on scripts

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#survey").submit(function(event) {
+    const name = $("#nameInput").val();
+    const food = $("#food").val();
+    const genre = $("input:radio[name=genre]:checked").val();
+    const faveDay = $("#faveDay").val();
+    const favoriteColor = $("#color").val();
+  });
+  function 
+});
