d9ea17c67c7533041d5dbc2b463506275bf948dd
jb <jessb.epicodus@gmail.com>
2022-01-12 16:08:48 -0800

    fix submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-  $("#survey").submit(function(event) {
+  $("#survey").submit(function() {
     const name = $("#nameInput").val();
     const food = $("#food").val();
     const genre = $("input:radio[name=genre]:checked").val();
     const faveDay = $("#faveDay").val();
     const favoriteColor = $("#color").val();
   });
-  function 
 });
